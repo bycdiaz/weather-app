b6c1a87f170c0ac64c0c32479d9bd390debb95e8
Carlos Diaz <cdiaz86@gmail.com>
2021-02-15 21:43:50 -0800

    Refectors logic for saving to db.

--- backend/app.py
+++ backend/app.py
@@ -16,13 +16,18 @@ def create_db():
 
     conn.execute(
         'CREATE TABLE IF NOT EXISTS weather (city TEXT UNIQUE ON CONFLICT REPLACE, feels_like REAL, description TEXT, icon TEXT)')
+    conn.execute(
+        'CREATE TABLE IF NOT EXISTS locationinfo (city TEXT UNIQUE ON CONFLICT REPLACE, notes TEXT)')
     print("Table created successfully")
 
     conn.close()
 
 
-def getWeather():
-    url = set_url('Dallas')
+create_db()
+
+
+def getWeather(city_name):
+    url = set_url(city_name)
     try:
         response = requests.get(url)
         json_response = response.json()
@@ -31,10 +36,9 @@ def getWeather():
         return f"Could not process request", 404
 
 
-@app.route('/', methods=['GET'])
-def send_to_db():
-    create_db()
-    api_response = getWeather()
+def save_weather_to_db(city_name):
+
+    api_response = getWeather(city_name)
     name, main, weather = [api_response[x]
                            for x in ('name', 'main', 'weather')]
     city = name
@@ -52,7 +56,23 @@ def send_to_db():
     return api_response
 
 
+def save_user_input_to_db(city, notes):
+    with sqlite3.connect('database.db') as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO locationinfo (city, notes) VALUES (?,?)",
+                    (city, notes))
+        con.commit()
+        msg = "Records added!"
+        print(msg)
+
+
+@app.route('/', methods=['GET'])
 @app.route('/forminput', methods=['POST'])
 def get_form_info():
-    print(request.json)
+    form_input = request.json
+
+    city, notes = [form_input[x]
+                   for x in ('cityName', 'notes')]
+    save_weather_to_db(city)
+    save_user_input_to_db(city, notes)
     return request.json
