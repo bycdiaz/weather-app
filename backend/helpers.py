d565acdd58659e832159b11605412e72108d32bd
Carlos Diaz <cdiaz86@gmail.com>
2021-02-15 19:04:16 -0800

    Allows input of city name.

--- backend/helpers.py
+++ backend/helpers.py
@@ -5,5 +5,5 @@ def set_key():
     return os.getenv("API_KEY")
 
 
-def set_url():
-    return f'https://api.openweathermap.org/data/2.5/weather?q=los angeles&appid={set_key()}&units=imperial'
+def set_url(city):
+    return f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={set_key()}&units=imperial'
