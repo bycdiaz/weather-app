0da686aa4009d225c52a3402c8e331a3e69378f0
Carlos Diaz <cdiaz86@gmail.com>
2021-02-16 22:41:26 -0800

    Removses function.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,10 @@ import Form from './components/Form';
 
 function App() {
   const [formState, setFormState] = useState();
-  function getFormState(state) {
-    return setFormState(state);
-  }
+
   return (
     <div className="App">
-      <Form getFormState={getFormState}/>
+      <Form />
     </div>
   );
 }
