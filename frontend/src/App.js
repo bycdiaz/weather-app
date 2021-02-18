d341bbb73e2dccfd1c4f1133f32ecc68a11c18de
Carlos Diaz <cdiaz86@gmail.com>
2021-02-17 19:47:51 -0800

    Imports destination list component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
+
 import Form from './components/Form';
+import DestinationsList from "./components/DestinationsList";
 
 function App() {
   const [formState, setFormState] = useState();
 
   return (
     <div className="App">
+      <h1>Dream Destinations</h1>
       <Form />
+      <DestinationsList />
     </div>
   );
 }
