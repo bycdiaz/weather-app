db67900abc1b5b2924b45c21158a2785e470fcc5
Carlos Diaz <cdiaz86@gmail.com>
2021-02-17 20:27:02 -0800

    Renders destination list.

--- frontend/src/components/DestinationsList.js
+++ frontend/src/components/DestinationsList.js
@@ -1,19 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
 function Destinations() {
-  const [destinationList, setDestinationList] = useState();
+  const [destinations, setDestinations] = useState();
 
   async function getDestinations() {
-    const response = await fetch('http://localhost:5000/dbinfo');
-    const json = await response.json();
-    setDestinationList({ data: json });
+    try {
+      const response = await fetch('http://localhost:5000/dbinfo');
+      const json = await response.json();
+      setDestinations(json);
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
     getDestinations()
   }, []);
 
-  return <p>wow</p>
+  function DestinationList() {
+    if (destinations) {
+      return destinations.map(destination => {
+        return (
+          <li>
+            <img
+              src={`http://openweathermap.org/img/wn/${destination.icon}@2x.png`}
+              alt="current weather icon"
+            />
+            <p>{destination.name}</p>
+            <p>{destination.description}</p>
+            <p>{`Feels Like: ${destination.feels_like}`}</p>
+            <p>{destination.notes}</p>
+          </li>
+        )
+      })
+    }
+  }
+
+  return (
+    <ul className="destinations">
+      <DestinationList />
+    </ul>
+  )
+
 }
 
 export default Destinations;
